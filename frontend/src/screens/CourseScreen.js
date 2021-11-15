f2a2b109ef5d57356a644451f7241d779d14b3a0
hrimG <hrimgandhi@gmail.com>
2021-11-15 23:20:38 +0530

    State management with Redux

--- frontend/src/screens/CourseScreen.js
+++ frontend/src/screens/CourseScreen.js
@@ -1,74 +1,82 @@
 import React, { useState, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Difficulty from '../components/Difficulty'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listCourseDetails } from '../actions/courseActions'
 
 function CourseScreen() {
     const params = useParams();    
-    const [course, setCourse] = useState([])
-
+    const dispatch = useDispatch()
+    const courseDetails = useSelector(state => state.courseDetails)
+    const { loading, error, course } = courseDetails
     useEffect(() => {
-        async function fetchCourse(){
-            const { data } = await axios.get(`/api/courses/${params.id}`)
-            setCourse(data)
-        }
-        fetchCourse()
-        
-    }, [params.id])
+        dispatch(listCourseDetails(params.id))
+    }, [params, dispatch])
     
     return (
         <div>
             <Link to='/' className='btn btn-light my-3'>Go Back</Link>
-            <Row>
-                <Col md={6}>
-                    <Image src={course.image} alt={course.name} />
-                </Col>
-                <Col md={3}>
-                    <ListGroup variant="flush">
-                        <ListGroup.Item>
-                            <h3 className="text-uppercase">{course.name}</h3>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Difficulty value={course.difficulty} text={`${course.numComments} comments`} color={'#f8e825'} />
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Tutor: {course.tutor}
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Description: {course.description}
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Tutor: </Col>
-                                    <Col>
-                                        <strong>{course.tutor}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Status: </Col>
-                                    <Col>
-                                        {course.lecturesScheduled > 0 ? 'Lectures Scheduled' : 'No upcoming Lectures'}
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item >
-                                <Row>
-                                    <Button className='btn btn-block align-items-center' disabled={course.lecturesScheduled === 0} type='button'>Scheduler</Button>
-                                </Row>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
+            {loading ?
+                <Loader />
+                : error 
+                    ? <Message variant='danger'>{error}</Message>
+                    :(
+                        <Row>
+                            <Col md={6}>
+                                <Image src={course.image} alt={course.name} />
+                            </Col>
+                            <Col md={3}>
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item>
+                                        <h3 className="text-uppercase">{course.name}</h3>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <Difficulty value={course.difficulty} text={`${course.numComments} comments`} color={'#f8e825'} />
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        Tutor: {course.tutor}
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        Description: {course.description}
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                            <Col md={3}>
+                                <Card>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item>
+                                            <Row>
+                                                <Col>Tutor: </Col>
+                                                <Col>
+                                                    <strong>{course.tutor}</strong>
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                        <ListGroup.Item>
+                                            <Row>
+                                                <Col>Status: </Col>
+                                                <Col>
+                                                    {course.lecturesScheduled > 0 ? 'Lectures Scheduled' : 'No upcoming Lectures'}
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                        <ListGroup.Item >
+                                            <Row>
+                                                <Button className='btn btn-block align-items-center' disabled={course.lecturesScheduled === 0} type='button'>Scheduler</Button>
+                                            </Row>
+                                        </ListGroup.Item>
+                                    </ListGroup>
+                                </Card>
+                            </Col>
+                        </Row>
+                    )
+
+
+            }
         </div>
     )
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,35 @@
 import React, { useState, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Course from '../components/Course'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listCourses } from '../actions/courseActions'
+import { courseListReducer } from '../reducers/courseReducers'
+
 function HomeScreen() {
-    const [courses, setCourses] = useState([])
+    const dispatch = useDispatch()
+    const courseList = useSelector(state => state.courseList)
+    const { error, loading, courses} = courseList
 
     useEffect(() => {
-        async function fetchCourses(){
-            const { data } = await axios.get('/api/courses/')
-            setCourses(data)
-        }
-
-        fetchCourses()
-        
-    }, [])
+        dispatch(listCourses())
+    }, [dispatch])
 
     return (
         <div>
             <h1> Latest Courses</h1>
-            <Row>
-                {courses.map(course =>(
-                    <Col key={course._id} sm={12} md={6} lg={4} xl={3}>
-                        <Course course = {course} />
-                    </Col>
-                ))}
-            </Row>
+            {loading ? <Loader /> 
+                : error ? <Message variant='danger'>{ error }</Message>
+                    :
+                    <Row>
+                        {courses.map(course =>(
+                            <Col key={course._id} sm={12} md={6} lg={4} xl={3}>
+                                <Course course = {course} />
+                            </Col>
+                        ))}
+                    </Row>
+            }
         </div>
     )
 }
